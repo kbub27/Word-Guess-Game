f127d2d07cc26fb3db74d21f19ace49f7de0619e
kbub27 <kalebmerriman27@gmail.com>
2019-03-09 00:15:58 -0800

    javascript added

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+const bands = ["nirvana", "pearl jam", "soundgarden", "everclear", "flowerhead", "smashing pumpkins", "stone temple pilots", "melvins"];
+
+
+var word = document.getElementById("word");
+var wins = document.getElementById("userWins");
+var losses = document.getElementById("userLosses");
+var alrdy = document.getElementById("guessesMade");
+var remaining = document.getElementById("guessesLeft");
+
+const game = {
+
+    win: 0,
+    lose: 0,
+    guessesLeft: 0,
+    choices: [],
+
+    generateWord: function() {
+        let randNum = Math.floor(Math.random() * bands.length);
+        let finalWord = bands[randNum];
+        console.log(finalWord);
+    },
+
+    userChoice: function() {
+
+        document.onkeyup = function(event) {
+            let userGuess = event.key.toLowerCase();
+            game.choices.push(userGuess);
+        }
+    },
+
+
+};
+
+game.generateWord();
+game.userChoice();
